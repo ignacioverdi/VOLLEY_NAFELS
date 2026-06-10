08b4a0d25b98c6f089bdcb2839efce55d4a89b0f
ignacioverdi <ignacio.verdi@gmail.com>
2026-06-10 14:53:36 -0300

    Add files via upload

---
+++ mapa_videos.js
@@ -0,0 +1,9 @@
+/* ===== MAPA DE VIDEOS =====
+   Por cada partido, pegá el link de YouTube al lado de su CÓDIGO (el número
+   del .dvw que pone VolleyMetrics). El sistema lee el código solo del .dvw.   */
+window.MAPA_VIDEOS = {
+  // código        link de YouTube
+  "751238": "https://youtu.be/1hi9AA9sHKs",  // ← video que subiste (confirmar que sea este partido)
+  "751235": "",
+  "751236": ""
+};
