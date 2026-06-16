f310126abf0452b8c37a062813d7d840b506dd71
Ignacio Verdi <ignacioverdi@users.noreply.github.com>
2026-06-16 08:55:51 -0300

    Actualizacion mar 16/06/2026

--- datos_equipo.js
+++ datos_equipo.js
@@ -1,21 +1,12 @@
 window.EQUIPO_DATA = {
-  "generado": "07/06/2026",
+  "generado": "16/06/2026",
   "jugadores": [
     {
       "num": 1,
-      "nombre": "DEECKE",
-      "pos": "ARMADOR",
-      "foto": "fotos/01.jpg",
-      "pais": "🇨🇭",
-      "altura": "",
-      "edad": 0
-    },
-    {
-      "num": 2,
-      "nombre": "FEUZ",
-      "pos": "CENTRAL",
+      "nombre": "DURDOS",
+      "pos": "PUNTA",
       "foto": null,
-      "pais": "🇨🇭",
+      "pais": "🇦🇷",
       "altura": "",
       "edad": 0
     },
@@ -39,25 +30,16 @@ window.EQUIPO_DATA = {
     },
     {
       "num": 5,
-      "nombre": "HESSELHOLT",
+      "nombre": "CLEMENT",
       "pos": "CENTRAL",
-      "foto": "fotos/05.jpg",
-      "pais": "🇩🇰",
-      "altura": "",
-      "edad": 0
-    },
-    {
-      "num": 6,
-      "nombre": "CABAÑAS",
-      "pos": "OPUESTO",
-      "foto": "fotos/06.jpg",
-      "pais": "🇦🇷",
+      "foto": null,
+      "pais": "🇨🇭",
       "altura": "",
       "edad": 0
     },
     {
       "num": 7,
-      "nombre": "SCHMID",
+      "nombre": "SCHMID R",
       "pos": "CENTRAL",
       "foto": "fotos/07.jpg",
       "pais": "🇨🇭",
@@ -65,20 +47,11 @@ window.EQUIPO_DATA = {
       "edad": 0
     },
     {
-      "num": 8,
-      "nombre": "PETER",
-      "pos": "LIBERO",
-      "foto": "fotos/08.jpg",
-      "pais": "🇨🇭",
-      "altura": "",
-      "edad": 0
-    },
-    {
       "num": 9,
-      "nombre": "BROCH",
-      "pos": "PUNTA",
-      "foto": "fotos/09.jpg",
-      "pais": "🇨🇭",
+      "nombre": "NORRIS",
+      "pos": "OPUESTO",
+      "foto": null,
+      "pais": "🇺🇸",
       "altura": "",
       "edad": 0
     },
@@ -87,7 +60,7 @@ window.EQUIPO_DATA = {
       "nombre": "BOGDANOVSKI",
       "pos": "PUNTA",
       "foto": "fotos/10.jpg",
-      "pais": "🇲🇰",
+      "pais": "🇨🇭",
       "altura": "",
       "edad": 0
     },
@@ -101,20 +74,38 @@ window.EQUIPO_DATA = {
       "edad": 0
     },
     {
-      "num": 14,
-      "nombre": "FIGUEIREDO",
-      "pos": "PUNTA",
-      "foto": "fotos/14.jpg",
-      "pais": "🇧🇷",
+      "num": 12,
+      "nombre": "JOHANSSON",
+      "pos": "CENTRAL",
+      "foto": null,
+      "pais": "🇸🇪",
       "altura": "",
       "edad": 0
     },
     {
-      "num": 15,
-      "nombre": "NIKOLOV",
-      "pos": "CENTRAL",
-      "foto": "fotos/15.jpg",
-      "pais": "🇲🇰",
+      "num": 13,
+      "nombre": "STEIMANN",
+      "pos": "ARMADOR",
+      "foto": null,
+      "pais": "🇨🇭",
+      "altura": "",
+      "edad": 0
+    },
+    {
+      "num": 17,
+      "nombre": "ROFFLER",
+      "pos": "PUNTA",
+      "foto": null,
+      "pais": "🇨🇭",
+      "altura": "",
+      "edad": 0
+    },
+    {
+      "num": 20,
+      "nombre": "SCHMID J",
+      "pos": "LIBERO",
+      "foto": null,
+      "pais": "🇨🇭",
       "altura": "",
       "edad": 0
     }
