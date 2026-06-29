a496d039b59ae64e86dfcae110ed65d0fa3f618d
Ignacio Verdi <ignacioverdi@users.noreply.github.com>
2026-06-29 11:57:40 -0300

    Actualizacion lun 29/06/2026

--- datos_partidos.js
+++ datos_partidos.js
@@ -1,5 +1,5 @@
-// datos_partidos.js — 29/06/2026, 11:17:10
-const PARTIDOS_GENERADO = "29/06/2026, 11:17:10";
+// datos_partidos.js — 29/06/2026, 11:57:21
+const PARTIDOS_GENERADO = "29/06/2026, 11:57:21";
 const PARTIDOS_TOTAL = 0;
 const PARTIDOS_META = [];
 const PARTIDOS_JUGADORES = [];
